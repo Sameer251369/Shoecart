93d6cc5ba71647e7363fd9e53f27da873ccff775
Sameer251369 <sameerbashir522@gmail.com>
2025-12-24 22:47:47 +0530

    fix netlify blank page

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // 1. Import it
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/', // 👈 THIS IS THE KEY LINE
   plugins: [
     react(),
-    tailwindcss(), // 2. Add it here
+    tailwindcss(),
   ],
 })
